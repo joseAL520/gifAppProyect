5748543efe9cac9f896c10a50b7562c81b066ef8
JA <jose.albelt1817@gmail.com>
2023-12-11 12:04:08 -0500

    viewchild

--- src/app/gifs/components/serach-box/search-box.component.ts
+++ src/app/gifs/components/serach-box/search-box.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'gifs-search-boxcomponet',
     template: `
             <h5>Search:</h5>
-            <input type="text" class="form-control" placeholder="search gif"> `
+            <input type="text" class="form-control" placeholder="search gif" (keyup.enter)="searchTag()" #txtTagInput> `
     
 })
 
 export class SearchBoxComponent  {
+    
+@ViewChild('txtTagInput')
+public tagInput!: ElementRef<HTMLInputElement>; 
+    
     constructor() { }
+    
+
+    searchTag(){
+        const newTag = this.tagInput.nativeElement.value;
+            console.warn(newTag);
+
+    }
 }
